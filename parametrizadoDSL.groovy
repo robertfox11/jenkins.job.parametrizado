ad869cbc1f5641e85fe8c16a9efebef1268d77a9
Robert <48912628+robertfox11@users.noreply.github.com>
2023-07-23 01:02:26 +0200

    Create parametrizadoDSL.groovy

---
+++ parametrizadoDSL.groovy
@@ -0,0 +1,40 @@
+job('MiSegundoTrabajo') {
+    description('Este es un trabajo creado con Job DSL')
+  	scm {
+        git ('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') {
+      		node / gitConfigName('roberfox11')
+      		node / gitConfigEmail('11rsahome@gmail.com')
+        }
+    }
+    parameters {
+          stringParam('nombre', defaultValue = 'Robert', description = 'Parametro de cadena para el job')
+          choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+          booleanParam('agente', false)
+   }
+   triggers {
+          //cron('H/7 * * * *')
+   }
+   steps {
+          shell("bash jobscript.sh")
+   }
+   publishers {
+        mailer('reyfoxgame11@gmail.com', true, true)
+        slackNotifier {
+          notifyAborted(true)
+          notifyEveryFailure(true)
+          notifyNotBuilt(false)
+          notifyUnstable(false)
+          notifyBackToNormal(true)
+          notifySuccess(false)
+          notifyRepeatedFailure(false)
+          startNotification(false)
+          includeTestSummary(false)
+          includeCustomMessage(false)
+          customMessage(null)
+          sendAs(null)
+          commitInfoChoice('NONE')
+          teamDomain(null)
+          authToken(null)
+        }
+   }
+}
