a0462f44db60336ad8c53eaba3b3be32e6c29acb
Robert <48912628+robertfox11@users.noreply.github.com>
2023-07-24 12:12:35 +0200

    Update nodejsDSL.groovy

--- DslNode/nodejsDSL.groovy
+++ DslNode/nodejsDSL.groovy
@@ -1,7 +1,7 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/robertfox11/jenkins.job.parametrizado.git', 'master') { node ->
+        git('https://github.com/robertfox11/jenkins.job.parametrizado.git', 'main') { node ->
       		  node / gitConfigName('roberfox11')
       		  node / gitConfigEmail('11rsahome@gmail.com')
         }
