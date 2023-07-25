3d584308a3ff8d41eab9beffc3c9e243c0ace2f4
Robert <48912628+robertfox11@users.noreply.github.com>
2023-07-25 19:25:16 +0200

    Create nodejsDockerDSL.groovy
    Creamos de imagen y de job

---
+++ DslNode/nodejsDockerDSL.groovy
@@ -0,0 +1,44 @@
+job('Aplicacion Node.js Docker DSL') {
+    description('Aplicación Node JS Docker DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/robertfox11/jenkins.job.parametrizado.git', 'main') { node ->
+      		  node / gitConfigName('roberfox11')
+      		  node / gitConfigEmail('11rsahome@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        dockerBuildAndPublish {//subimos la imagen de docker 
+            repositoryName('reyfox11/nodejsapp')
+            tag('${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
